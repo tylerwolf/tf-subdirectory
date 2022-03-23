e179c28d5f6c9150ce77f180cb5beec49aafb842
Tyler Wolf <tdwolf@gmail.com>
2022-03-23 10:23:29 -0600

    Update main.tf

--- first-subdirectory/main.tf
+++ first-subdirectory/main.tf
@@ -10,3 +10,8 @@ resource "null_resource" "second" {}
 
 
 resource "null_resource" "another" {}
+
+resource "fakewebservices_vpc" "primary_vpc" {
+  name       = "Primary VPC"
+  cidr_block = "0.0.0.0/1"
+}
